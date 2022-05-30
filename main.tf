d0d010459b32402da3ec73b9daa133d9699dad22
Mark Allan <markxa@markxa.com>
2022-05-30 17:31:05 +0100

    Update SQL provider

--- main.tf
+++ main.tf
@@ -7,24 +7,23 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-resource "azurerm_sql_server" "sqlserver" {
+resource "azurerm_mssql_server" "sqlserver" {
   name                         = "mxa-raspsqlserver"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "mradministrator"
-  administrator_login_password = "thisIsDog11"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "thePassword123!"
 
   tags = {
     customer = "allsop"
   }
 }
 
-resource "azurerm_sql_database" "sqlserver" {
-  name                = "raspdb"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sqlserver.name
+resource "azurerm_mssql_database" "sqlserver" {
+  name      = "raspdb"
+  server_id = azurerm_mssql_server.sqlserver.id
+  sku_name  = "Basic"
 
   tags = {
     customer = "allsop"
