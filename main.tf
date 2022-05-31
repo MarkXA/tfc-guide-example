ea1b81ec2c77d1d37525c4edc9ebf1373f55f6a6
Mark Allan <markxa@markxa.com>
2022-05-31 11:27:06 +0100

    Remove tags from role assignment

--- main.tf
+++ main.tf
@@ -106,8 +106,4 @@ resource "azurerm_role_assignment" "example" {
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
-
-  tags = {
-    customer = "allsop"
-  }
 }
