a94fd03b751e942971071d5b7559a66b22b7bb44
Mark Allan <markxa@markxa.com>
2022-05-31 12:25:22 +0100

    Fix resource name

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-resource "azurerm_role_assignment" "example" {
+resource "azurerm_role_assignment" "aksacrpull" {
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
