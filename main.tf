a32c57ce0fa6d38de1c59367ad9ffffcf90571be
Mark Allan <markxa@markxa.com>
2022-05-31 11:26:20 +0100

    Remove hyphen not allowed in ACR name

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "azurerm_application_insights" "appinsights" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "mxa-raspacr"
+  name                = "mxaraspacr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
