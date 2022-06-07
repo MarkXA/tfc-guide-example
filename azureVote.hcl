ae432c41af78082c579780a9bea125d3ab19ead0
Mark Allan <markxa@markxa.com>
2022-06-07 10:39:27 +0100

    Try HCL not YAML

---
+++ azureVote.hcl
@@ -0,0 +1,185 @@
+resource "kubernetes_manifest" "deployment_azure_vote_back" {
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind" = "Deployment"
+    "metadata" = {
+      "name" = "azure-vote-back"
+    }
+    "spec" = {
+      "replicas" = 1
+      "selector" = {
+        "matchLabels" = {
+          "app" = "azure-vote-back"
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "app" = "azure-vote-back"
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "env" = [
+                {
+                  "name" = "ALLOW_EMPTY_PASSWORD"
+                  "value" = "yes"
+                },
+              ]
+              "image" = "mcr.microsoft.com/oss/bitnami/redis:6.0.8"
+              "name" = "azure-vote-back"
+              "ports" = [
+                {
+                  "containerPort" = 6379
+                  "name" = "redis"
+                },
+              ]
+              "resources" = {
+                "limits" = {
+                  "cpu" = "250m"
+                  "memory" = "256Mi"
+                }
+                "requests" = {
+                  "cpu" = "100m"
+                  "memory" = "128Mi"
+                }
+              }
+            },
+          ]
+          "nodeSelector" = {
+            "beta.kubernetes.io/os" = "linux"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "service_azure_vote_back" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Service"
+    "metadata" = {
+      "name" = "azure-vote-back"
+    }
+    "spec" = {
+      "ports" = [
+        {
+          "port" = 6379
+        },
+      ]
+      "selector" = {
+        "app" = "azure-vote-back"
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "service_azure_vote_front" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Service"
+    "metadata" = {
+      "name" = "azure-vote-front"
+    }
+    "spec" = {
+      "ports" = [
+        {
+          "port" = 80
+        },
+      ]
+      "selector" = {
+        "app" = "azure-vote-front"
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "deployment_azure_vote_front" {
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind" = "Deployment"
+    "metadata" = {
+      "name" = "azure-vote-front"
+    }
+    "spec" = {
+      "replicas" = 1
+      "selector" = {
+        "matchLabels" = {
+          "app" = "azure-vote-front"
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "app" = "azure-vote-front"
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "env" = [
+                {
+                  "name" = "REDIS"
+                  "value" = "azure-vote-back"
+                },
+              ]
+              "image" = "mcr.microsoft.com/azuredocs/azure-vote-front:v1"
+              "name" = "azure-vote-front"
+              "ports" = [
+                {
+                  "containerPort" = 80
+                },
+              ]
+              "resources" = {
+                "limits" = {
+                  "cpu" = "250m"
+                  "memory" = "256Mi"
+                }
+                "requests" = {
+                  "cpu" = "100m"
+                  "memory" = "128Mi"
+                }
+              }
+            },
+          ]
+          "nodeSelector" = {
+            "beta.kubernetes.io/os" = "linux"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "ingress_azure_vote" {
+  manifest = {
+    "apiVersion" = "extensions/v1beta1"
+    "kind" = "Ingress"
+    "metadata" = {
+      "annotations" = {
+        "kubernetes.io/ingress.class" = "azure/application-gateway"
+      }
+      "name" = "azure-vote"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "rules" = [
+        {
+          "http" = {
+            "paths" = [
+              {
+                "backend" = {
+                  "serviceName" = "azure-vote-front"
+                  "servicePort" = 80
+                }
+                "path" = "/"
+              },
+            ]
+          }
+        },
+      ]
+    }
+  }
+}
--- main.tf
+++ main.tf
@@ -128,15 +128,201 @@ resource "helm_release" "nginx_ingress" {
   }
 }
 
-resource "kubernetes_manifest" "deployment-back" {
-  manifest = yamldecode(file("azureVoteDeploymentBack.yaml"))
+resource "kubernetes_manifest" "deployment_azure_vote_back" {
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind" = "Deployment"
+    "metadata" = {
+      "name" = "azure-vote-back"
+    }
+    "spec" = {
+      "replicas" = 1
+      "selector" = {
+        "matchLabels" = {
+          "app" = "azure-vote-back"
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "app" = "azure-vote-back"
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "env" = [
+                {
+                  "name" = "ALLOW_EMPTY_PASSWORD"
+                  "value" = "yes"
+                },
+              ]
+              "image" = "mcr.microsoft.com/oss/bitnami/redis:6.0.8"
+              "name" = "azure-vote-back"
+              "ports" = [
+                {
+                  "containerPort" = 6379
+                  "name" = "redis"
+                },
+              ]
+              "resources" = {
+                "limits" = {
+                  "cpu" = "250m"
+                  "memory" = "256Mi"
+                }
+                "requests" = {
+                  "cpu" = "100m"
+                  "memory" = "128Mi"
+                }
+              }
+            },
+          ]
+          "nodeSelector" = {
+            "beta.kubernetes.io/os" = "linux"
+          }
+        }
+      }
+    }
+  }
 }
-resource "kubernetes_manifest" "deployment-front" {
-  manifest = yamldecode(templatefile("azureVoteDeploymentFront.yaml", { FRONTNAME = var.FRONTNAME }))
+
+resource "kubernetes_manifest" "service_azure_vote_back" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Service"
+    "metadata" = {
+      "name" = "azure-vote-back"
+    }
+    "spec" = {
+      "ports" = [
+        {
+          "port" = 6379
+        },
+      ]
+      "selector" = {
+        "app" = "azure-vote-back"
+      }
+    }
+  }
 }
-resource "kubernetes_manifest" "service-back" {
-  manifest = yamldecode(file("azureVoteServiceBack.yaml"))
+
+resource "kubernetes_manifest" "service_azure_vote_front" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Service"
+    "metadata" = {
+      "name" = var.FRONTNAME
+    }
+    "spec" = {
+      "ports" = [
+        {
+          "port" = 80
+        },
+      ]
+      "selector" = {
+        "app" = var.FRONTNAME
+      }
+    }
+  }
 }
-resource "kubernetes_manifest" "service-front" {
-  manifest = yamldecode(templatefile("azureVoteServiceFront.yaml", { FRONTNAME = var.FRONTNAME }))
+
+resource "kubernetes_manifest" "deployment_azure_vote_front" {
+  manifest = {
+    "apiVersion" = "apps/v1"
+    "kind" = "Deployment"
+    "metadata" = {
+      "name" = var.FRONTNAME
+    }
+    "spec" = {
+      "replicas" = 1
+      "selector" = {
+        "matchLabels" = {
+          "app" = var.FRONTNAME
+        }
+      }
+      "template" = {
+        "metadata" = {
+          "labels" = {
+            "app" = var.FRONTNAME
+          }
+        }
+        "spec" = {
+          "containers" = [
+            {
+              "env" = [
+                {
+                  "name" = "REDIS"
+                  "value" = "azure-vote-back"
+                },
+              ]
+              "image" = "mcr.microsoft.com/azuredocs/azure-vote-front:v1"
+              "name" = var.FRONTNAME
+              "ports" = [
+                {
+                  "containerPort" = 80
+                },
+              ]
+              "resources" = {
+                "limits" = {
+                  "cpu" = "250m"
+                  "memory" = "256Mi"
+                }
+                "requests" = {
+                  "cpu" = "100m"
+                  "memory" = "128Mi"
+                }
+              }
+            },
+          ]
+          "nodeSelector" = {
+            "beta.kubernetes.io/os" = "linux"
+          }
+        }
+      }
+    }
+  }
 }
+
+resource "kubernetes_manifest" "ingress_azure_vote" {
+  manifest = {
+    "apiVersion" = "extensions/v1beta1"
+    "kind" = "Ingress"
+    "metadata" = {
+      "annotations" = {
+        "kubernetes.io/ingress.class" = "azure/application-gateway"
+      }
+      "name" = "azure-vote"
+      "namespace" = "default"
+    }
+    "spec" = {
+      "rules" = [
+        {
+          "http" = {
+            "paths" = [
+              {
+                "backend" = {
+                  "serviceName" = var.FRONTNAME
+                  "servicePort" = 80
+                }
+                "path" = "/"
+              },
+            ]
+          }
+        },
+      ]
+    }
+  }
+}
+
+# resource "kubernetes_manifest" "deployment-back" {
+#   manifest = yamldecode(file("azureVoteDeploymentBack.yaml"))
+# }
+# resource "kubernetes_manifest" "deployment-front" {
+#   manifest = yamldecode(templatefile("azureVoteDeploymentFront.yaml", { FRONTNAME = var.FRONTNAME }))
+# }
+# resource "kubernetes_manifest" "service-back" {
+#   manifest = yamldecode(file("azureVoteServiceBack.yaml"))
+# }
+# resource "kubernetes_manifest" "service-front" {
+#   manifest = yamldecode(templatefile("azureVoteServiceFront.yaml", { FRONTNAME = var.FRONTNAME }))
+# }
